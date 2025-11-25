9e1fdbbc083e291bba52ebcacd2a7bc99889a8e0
Sciborgue <alexis.arricca@protonmail.com>
2025-11-25 14:39:51 +0000

    Dataset+workspace

--- src/model/train.py
+++ src/model/train.py
@@ -8,6 +8,7 @@ import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from azureml.core import Dataset, Workspace
 
 
 # define functions

161779fe2c9766594e07e3192b51ccd32057b2fe
Sciborgue <alexis.arricca@protonmail.com>
2025-11-26 15:29:54 +0000

    noarob

--- src/model/train.py
+++ src/model/train.py
@@ -9,7 +9,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
-# define functions
+
 def main(args):
     # TO DO: enable autologging
     mlflow.autolog()
